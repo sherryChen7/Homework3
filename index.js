0dc1d53fc6e4281344eb23663f22e41f7d763b72
irons <irons@ironsde-MacBook-Pro.local>
2018-12-09 16:26:04 +0800

    Restructing

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const dbDebugger = require('debug')('app:db')
 // const config = require('configurarion')
 const morgan = require('morgan')
 const helmet = require('helmet')
-const Joi = require('Joi')
+
+const courses = require('./routes/courses')
+const home = require('./routes/home')
 const express = require('express')
 const app = express()
 
@@ -17,6 +19,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static('public'))
 app.use(helmet())
+app.use('/api/courses', courses)
+app.use('/', home)
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'))
@@ -28,92 +32,6 @@ app.use(function(req, res, next){
     console.log('Authentication...')
     next()
 })
-
-const courses = [
-    {id:1, name:'course1'},
-    {id:2, name:'course2'},
-    {id:3, name:'course3'}
-]
-
-app.get('/', (req, res) => {
-    res.render('index', {title:'My Express APP!!', message: 'Hello'})
-})
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses)
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) //404
-        return res.status(404).send('The course is not find.')
-    res.send(course)
-})
-
-app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.validate(req.body, schema)
-    console.log(result)
-    if(result.error) {
-        return res.send(result.error.details[0].message)
-    }
-
-    if (!req.body.name || req.body.name.length <3)
-        res.status(404).send('aa')
-    const course = {
-        id: courses.length +1,
-        name: req.body.name
-    }
-    courses.push(course)
-    res.send(course)
-})
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) {//404
-        res.status(404).send('The course is not find.')
-        return res.send(course)
-    }
-
-    // const result = validateCourse(req.body)
-    // console.log(result)
-    const { error } = validateCourse(req.body) // result.error
-    if(error) {
-        return res.status(404).send(error.details[0].message)
-    }
-
-    course.name = req.body.name
-    res.send(course)
-})
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) {//404
-        res.status(404).send('The course is not find.')
-        return res.send(course)
-    }
-
-    // const result = validateCourse(req.body)
-    // console.log(result)
-    const { error } = validateCourse(req.body) // result.error
-    if(error) {
-        return res.status(404).send(error.details[0].message)
-    }
-
-    const i = courses.indexOf(course)
-    courses.splice(i, 1)
-    res.send(course)
-})
-
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(course, schema)
-}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listen ${port} ...`))
---
+++ routes/courses.js
@@ -0,0 +1,90 @@
+const Joi = require('Joi')
+const express = require('express')
+const router = express.Router()
+
+// console.log(`NODE_ENV: + ${process.env.NODE_ENV}`)
+// console.log(`app: ${router.get('env')}`)
+
+const courses = [
+    {id:1, name:'course1'},
+    {id:2, name:'course2'},
+    {id:3, name:'course3'}
+]
+
+router.get('/', (req, res) => {
+    res.send(courses)
+})
+
+router.get('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) //404
+        return res.status(404).send('The course is not find.')
+    res.send(course)
+})
+
+router.post('/', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema)
+    console.log(result)
+    if(result.error) {
+        return res.send(result.error.details[0].message)
+    }
+
+    if (!req.body.name || req.body.name.length <3)
+        res.status(404).send('aa')
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
+router.put('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) {//404
+        res.status(404).send('The course is not find.')
+        return res.send(course)
+    }
+
+    // const result = validateCourse(req.body)
+    // console.log(result)
+    const { error } = validateCourse(req.body) // result.error
+    if(error) {
+        return res.status(404).send(error.details[0].message)
+    }
+
+    course.name = req.body.name
+    res.send(course)
+})
+
+router.delete('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) {//404
+        res.status(404).send('The course is not find.')
+        return res.send(course)
+    }
+
+    // const result = validateCourse(req.body)
+    // console.log(result)
+    const { error } = validateCourse(req.body) // result.error
+    if(error) {
+        return res.status(404).send(error.details[0].message)
+    }
+
+    const i = courses.indexOf(course)
+    courses.splice(i, 1)
+    res.send(course)
+})
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema)
+}
+
+module.exports = router
