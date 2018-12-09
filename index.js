f528100377779c6b8de6515bea51084b56625361
irons <irons@ironsde-MacBook-Pro.local>
2018-12-09 21:57:09 +0800

    Fixed morgan not work issue

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const dbDebugger = require('debug')('app:db')
 // const config = require('configurarion')
 const morgan = require('morgan')
 const helmet = require('helmet')
-
 const courses = require('./routes/courses')
 const home = require('./routes/home')
 const express = require('express')
@@ -19,11 +18,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static('public'))
 app.use(helmet())
-app.use('/api/courses', courses)
-app.use('/', home)
 
 if (app.get('env') === 'development') {
-    app.use(morgan('tiny'))
+    app.use(morgan('dev'))
     console.log('Morgan enabled...')
     startupDebugger()
 }
@@ -32,6 +29,9 @@ app.use(function(req, res, next){
     console.log('Authentication...')
     next()
 })
+
+app.use('/api/courses', courses)
+app.use('/', home)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listen ${port} ...`))
