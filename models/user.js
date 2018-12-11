8502dda8b3093edb5140c9f254b562f75c45a569
sherry <sherry>
2018-12-11 21:28:18 +0800

    add user

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Joi = require('Joi')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength:5,
+        maxlength:255
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength:5,
+        maclength:255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength:5,
+        maxlength:1024
+    }
+})
+
+const User = mongoose.model('User', providerSchema)
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(244).required()
+    };
+    return Joi.validate(course, schema)
+}
+
+module.exports.User = User
+module.exports.validate = validateUser
