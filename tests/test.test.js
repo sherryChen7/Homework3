1bbf2a51fb01fa5b8ff49d1d1be544ee5ae5b33d
sherry <you@example.com>
2019-01-08 10:13:16 +0800

    modify gitignore file

--- tests/test.test.js
+++ tests/test.test.js
@@ -3,17 +3,49 @@
 //     throw new Error('xxx')
 // })
 
-test('absolute - should return a positive number if input is positive', () => {
-    const result = 1
-    expect(result).toBe(1)
+describe('absolute', () => {
+    it('should return a positive number if input is positive', () => {
+        const result = 1
+        expect(result).toBe(1)
+    })
+
+    it('should return a positive number if input is negative', () => {
+        const result = -1
+        expect(result).toBe(-1)
+    })
+
+    it('should return a positive number if input is 0', () => {
+        const result = 0
+        expect(result).toBe(0)
+    })
 })
 
-test('absolute - should return a positive number if input is negative', () => {
-    const result = -1
-    expect(result).toBe(-1)
+describe('greet', () => {
+    it('string', () => {
+        const result = 'ABCD'
+        expect(result).toMatch(/ABC/)
+        expect(result).toContain('ABC')
+    })
 })
 
-test('absolute - should return a positive number if input is 0', () => {
-    const result = 0
-    expect(result).toBe(0)
+describe('getCurrencies', () => {
+    it('should supported', () => {
+        const result = ['A', 'B']
+
+        expect(result[0]).toBe('A')
+    })
+})
+
+AAA {
+    'id' = ''
+}
+
+describe('getObjects', () => {
+    it('should Object', () => {
+        const result = new Object;
+
+        expect(result[0]).toBe('A')
+        expect(result[0]).toBe('A')
+
+    })
 })
